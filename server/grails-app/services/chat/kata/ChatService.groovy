c6c73b7c5c40921f58b9da707cecd0e1f3d43f7f
armando91 <ar.ferrero@gmail.com>
2013-06-25 10:24:35 +0200

    Server revisado

--- server/grails-app/services/chat/kata/ChatService.groovy
+++ server/grails-app/services/chat/kata/ChatService.groovy
@@ -5,40 +5,50 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 
 class ChatService {
 
-    private ReentrantReadWriteLock _rwl = new ReentrantReadWriteLock()
-    private Lock _r_lock = _rwl.readLock()
-    private Lock _w_lock = _rwl.writeLock()
-    private Collection<ChatMessage> _myCollection = new ArrayList<ChatMessage>()
+	private ReentrantReadWriteLock _rwl = new ReentrantReadWriteLock()
+	private Lock _r_lock = _rwl.readLock()
+	private Lock _w_lock = _rwl.writeLock()
+	private Collection<ChatMessage> _myCollection = new ArrayList<ChatMessage>()
 
-    /**
-     * Collects chat messages in the provided collection
-     * 
-     * @param if specified messages are collected from the provided sequence (exclusive)
-     * @param messages the collection where to add collected messages
-     * 
-     * @return the sequence of the last message collected.
-     */
-    Integer collectChatMessages(Collection<ChatMessage> collector, Integer fromSeq = null){
-        _r_lock.lock()        
-        Integer value = fromSeq == null ? 0 : fromSeq + 1
-        Integer size = _myCollection.size()
-        while(value < size){
-            collector.add(_myCollection.getAt(value++));
-        }
-        _r_lock.unlock()
-        return value -1
-    }
+	/**
+	 * Collects chat messages in the provided collection
+	 * 
+	 * @param if specified messages are collected from the provided sequence (exclusive)
+	 * @param messages the collection where to add collected messages
+	 * 
+	 * @return the sequence of the last message collected.
+	 */
+	Integer collectChatMessages(Collection<ChatMessage> collector, Integer fromSeq = null){
+		_r_lock.lock()
+		Integer value = fromSeq == null ? 0 : fromSeq + 1
+		Integer size = _myCollection.size()
+		try{
+			while(value < size){
+				collector.add(_myCollection.getAt(value++));
+			}
+		}catch (Exception e){
+			log.error("Exception collectChatMessages: "+e.printStackTrace());
+		}finally{
+			_r_lock.unlock();
+		}
+		return value -1
+	}
 
-    /**
-     * Puts a new message at the bottom of the chat
-     * 
-     * @param message the message to add to the chat
-     */
-    void putChatMessage(ChatMessage message){
-        _w_lock.lock()
-        if(message != null){
-            _myCollection.add(message);
-        }
-        _w_lock.unlock()
-    }
+	/**
+	 * Puts a new message at the bottom of the chat
+	 * 
+	 * @param message the message to add to the chat
+	 */
+	void putChatMessage(ChatMessage message){
+		_w_lock.lock()
+		try{
+			if(message != null){
+				_myCollection.add(message);
+			}
+		}catch (Exception e){
+			log.error("Exception  putChatMessage: "+ e.printStackTrace());
+		}finally{
+			_w_lock.unlock();
+		}
+	}
 }
