461d5929cb4cd1a655c59af10cbf7c2db7c4dc13
armando91 <ar.ferrero@gmail.com>
2013-06-27 12:36:29 +0200

    Añadido ciclo de vida de la aplicación, control de los string del interfaz mediande strings.xml.

--- client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/ChatActivity.java
@@ -132,4 +132,36 @@ public class ChatActivity extends Activity {
 		}).start();
 
 	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// mp.pause();
+		super.onPause();
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
--- client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
@@ -66,38 +66,38 @@ public class LoginActivity extends Activity {
 	}
 	
 	private void exit(View view){
-		finish();
+		Toast toast= Toast.makeText(getApplicationContext(), "Bye", Toast.LENGTH_SHORT );
+//		finish();
 	}
 	
-	@Override
-	protected void onStart() {
-		super.onStart();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
-
-	@Override
-	protected void onPause() {
-		// mp.pause();
-		super.onPause();
-
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-	}
-
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
+//	@Override
+//	protected void onStart() {
+//		super.onStart();
+//	}
+//
+//	@Override
+//	protected void onResume() {
+//		super.onResume();
+//	}
+//
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//
+//	}
+//
+//	@Override
+//	protected void onStop() {
+//		super.onStop();
+//	}
+//
+//	@Override
+//	protected void onRestart() {
+//		super.onRestart();
+//	}
+//
+//	@Override
+//	protected void onDestroy() {
+//		super.onDestroy();
+//	}
 }
