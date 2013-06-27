a01603a026f6f1d5705b487038f75bfaf8722713
armando91 <ar.ferrero@gmail.com>
2013-06-27 12:46:52 +0200

    Control para cerrar la aplicación

--- client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
@@ -65,39 +65,40 @@ public class LoginActivity extends Activity {
 		}
 	}
 	
-	private void exit(View view){
+	public void salir(View view){
 		Toast toast= Toast.makeText(getApplicationContext(), "Bye", Toast.LENGTH_SHORT );
-//		finish();
+		toast.show();
+		finish();
 	}
 	
-//	@Override
-//	protected void onStart() {
-//		super.onStart();
-//	}
-//
-//	@Override
-//	protected void onResume() {
-//		super.onResume();
-//	}
-//
-//	@Override
-//	protected void onPause() {
-//		super.onPause();
-//
-//	}
-//
-//	@Override
-//	protected void onStop() {
-//		super.onStop();
-//	}
-//
-//	@Override
-//	protected void onRestart() {
-//		super.onRestart();
-//	}
-//
-//	@Override
-//	protected void onDestroy() {
-//		super.onDestroy();
-//	}
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
