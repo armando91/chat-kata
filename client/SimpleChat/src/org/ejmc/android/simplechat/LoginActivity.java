e6d29707db337e59be7e2ee5c68bdbb1d539541f
armando91 <ar.ferrero@gmail.com>
2013-06-27 09:42:15 +0200

    Mejoras en el interfaz

--- client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
@@ -64,4 +64,40 @@ public class LoginActivity extends Activity {
 			notification.show();
 		}
 	}
+	
+	private void exit(View view){
+		finish();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// mp.pause();
+		super.onPause();
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
